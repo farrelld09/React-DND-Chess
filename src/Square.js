8bbe4bf2bb26cd28ba364db98154ac050abb5ca1
Dan Farrell <farrelld09@gmail.com>
2018-02-17 17:14:42 -0500

    updating board display

--- src/Square.js
+++ src/Square.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Knight from './Knight';
 
 export default class Square extends Component {
   static propTypes = {
     black: PropTypes.bool
   };
 
-  render() {
-    const { black } = this.props;
-    const fill = black ? 'black' : 'white';
-    const stroke = black ? 'white' : 'black';
+  renderSquare(x, y) {
+  const black = (x + y) % 2 === 1;
 
+  const [knightX, knightY] = this.props.knightPosition;
+  const piece = (x === knightX && y === knightY) ?
+    <Knight /> :
+    null;
+
+  return (
+    <Square black={black}>
+      {piece}
+    </Square>
+  );
+}
+
+
+  render() {
     return (
       <div style={{
-        backgroundColor: fill,
-        color: stroke,
         width: '100%',
         height: '100%'
       }}>
-        {this.props.children}
+        {this.renderSquare(0, 0)}
+        {this.renderSquare(1, 0)}
+        {this.renderSquare(2, 0)}
       </div>
     );
   }
